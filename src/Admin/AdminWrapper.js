d9748ff2f0e6ea382909de9069a187658524c563
Ekta Thakkar <ektathakkar.2407@gmail.com>
2021-01-10 15:20:13 +0530

    Submit button added for forms.

--- src/Admin/AdminWrapper.js
+++ src/Admin/AdminWrapper.js
@@ -1,10 +1,12 @@
-import { Typography } from "@material-ui/core";
-import React from "react";
+import { Typography,Button} from "@material-ui/core";
+import React,{useState} from "react";
 import Form from "../Form";
 import JSON from "../formJson.json";
 import MultiData from "../Form/MultiForm";
+import DataObj from "../Data"
 
 function AdminWrapper(props) {
+  const[submit,setSubmit]=useState({})
   const design = "col-lg-6 col-md-6 col-sm-12";
   if (props.selectedMenu === 1) {
     let obj = {};
@@ -12,6 +14,7 @@ function AdminWrapper(props) {
       console.log(value, id);
       obj[id] = value;
     };
+    
     return (
       <div>
         <Form
@@ -25,17 +28,34 @@ function AdminWrapper(props) {
   } else if (props.selectedMenu === 2) {
     return <div>123</div>;
   } else if (props.selectedMenu === 3) {
+    
     let obj = {};
     const formChange = (value, id) => {
       console.log(value, id);
       obj[id] = value;
     };
+    const handleSubmitclick=()=>{
+      console.log("obj",obj)
+      setSubmit(obj)
+    }
     return (
       <div>
         <div className="m-2" style={{ width: "100%", textAlign: "center" }}>
           <Typography variant="h5"> Company Info</Typography>
         </div>
         <Form fldArr={JSON["CompanyInfo"].fields} onChange={formChange} />
+        <div style={{padding:"1%"}}>
+        <Button
+        className="button"
+        variant="contained"
+        onClick={handleSubmitclick}
+        style={{width:"15%",backgroundColor:DataObj.backGroundColor,color:DataObj.fontColor}}
+      >
+       Submit
+      </Button>
+
+        </div>
+       
       </div>
     );
   }
